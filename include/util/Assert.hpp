cad7f3567bddae074612ef0912b2289e88777dc9
Daniel Mueller <deso@posteo.net>
2015-12-06 23:26:02 -0800

    Add exception specifier to __assert_fail
    When directly or indirectly including cassert, the C++ standard header
    for assertions, one implicitly pulls in a declaration of __assert_fail
    that contains and empty exception specifier "throw()". Unfortunately,
    this declaration conflicts with the declaration provided by this
    library.
    This patch adjusts this library's declaration to contain the empty
    exception specifier as well. Note that long term the usage of cassert is
    discouraged but in the interim (and even once nobody uses the standard
    header anymore), having this exception specifier has no foreseeable
    negative impact.

--- include/util/Assert.hpp
+++ include/util/Assert.hpp
@@ -38,7 +38,7 @@
 extern "C" void __assert_fail(char const* assertion,
                               char const* file,
                               unsigned int line,
-                              char const* function);
+                              char const* function) throw();
 #  define ASSERT(condition_)\
      ASSERT_IMPL(condition_, __assert_fail)
 #  define ASSERTOP(first_, operation_, second_)\
